4b65ec678e594e6333bad2b51b536e5031502f16
angelomadrid <angelo.madrid@zafra.cloud>
2022-02-04 04:52:21 -0600

    Struct User

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/amadrid196/go-structs.git/users"
+)
+
+func main() {
+	martha := users.User{Id: 1, Name: "Maria", Age: 20}
+	pedro := users.User{Id: 2, Name: "Pedro", Age: 22}
+	carlos := users.User{Id: 3, Name: "Carlos", Age: 26}
+	martha.SayHello()
+	martha.AddFriend(pedro)
+	martha.AddFriend(carlos)
+
+	martha.ListFriends()
+
+	martha.RemoveFriend(10)
+
+	fmt.Println("================================")
+
+	martha.ListFriends()
 }
--- users/users.go
+++ users/users.go
@@ -1,7 +1,44 @@
 package users
 
+import "fmt"
+
 type User struct {
-	id   int
-	name string
-	age  int
+	Id      int
+	Name    string
+	Age     int
+	frineds []User
+}
+
+func (u User) SayHello() {
+	fmt.Println("Hola me llamo", u.Name)
+}
+
+func (u *User) AddFriend(friend User) {
+	u.frineds = append(u.frineds, friend)
+}
+
+func (u User) ListFriends() {
+
+	for i, f := range u.frineds {
+		fmt.Printf("%d. %s\n", i+1, f.Name)
+	}
+}
+
+func (u User) findFriend(Id int) int {
+	for i, f := range u.frineds {
+		if f.Id == Id {
+			return i
+		}
+	}
+	return -1
+}
+
+func (u *User) RemoveFriend(Id int) {
+	index := u.findFriend(Id)
+
+	if index < 0 {
+		return
+	}
+
+	u.frineds = append(u.frineds[:index], u.frineds[index+1:]...)
 }
